e9bd7d82a060a96a87868f404af5c6cbe6e6c8a5
pockocmoc <pockocmoc@inbox.ru>
2022-07-06 21:55:24 +0300

    Игра чепуха.

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class MadLibs {
-}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+            System.out.println("Укажите любой цвет:");
+            String color = in.nextLine();
+
+            System.out.println("Укажите глагол, в прошедшем времени:");
+            String pastTenseVerb = in.nextLine();
+            System.out.println("Укажите прилагательное мужского рода в родительном падеже (например: глупого)");
+            String adjective = in.nextLine();
+
+
+
+            System.out.println(color + " дракон " + pastTenseVerb + " на " + adjective + " рыцаря.");
+            in.close();
+
+        }
+
+    }
